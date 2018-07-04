12554fedae12e9d1ff25d78f136f57543e7e96da
TienLS <neotien200065@gmail.com>
2018-07-04 18:36:12 +0700

    Chương 9:: 9.2.5:: Ajax.

---
+++ ch09/02-ajax/04-ajax/files/data-02.php
@@ -0,0 +1,12 @@
+<?php 
+    $course = array(
+        "id"    => 2,
+        "name"  => "PHP"
+    );
+
+    // echo "<pre>";
+    // print_r($course);
+    // echo "</pre>";
+
+    echo json_encode($course);
+?>
